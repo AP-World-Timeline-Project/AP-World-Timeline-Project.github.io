006ce92e31e24509061ab317b92754f7e70770a2
William Qin <williamqin@MacBook-Pro.local>
2019-12-16 23:26:55 -0600

    2nd commit

--- action.js
+++ action.js
@@ -1,9 +1,27 @@
 
+const UNITS = {
+	'0' : 'History Before 1200',
+	'1' : 'The Global Tapestry (1200&ndash;1450)',
+	'2' : 'Networks of Exchange (1200&ndash;1450)',
+	'3' : 'Land-Based Empires (1450&ndash;1750)',
+	'4' : 'Transoceanic Interconnections (1450&ndash;1750)',
+	'5' : 'Revolutions (1750&ndash;1900)'
+};
+
 $(document).on('click', '.bar, .dot', function (e) {
 	let scroll = $('article');
 	let tapTarget = $(e.currentTarget);
-	scroll.find('h5').html(tapTarget.find('h2').text() + ` [&nbsp;${tapTarget.find('time').text()}&nbsp;]`);
-	scroll.find('p').html(tapTarget.find('p').text());
+	scroll.find('.article-heading').html(`<h5>${tapTarget.find('h2').text()}</h5><h6>${tapTarget.find('time').text()}</h6>`);
+	scroll.find('.content').html(
+		tapTarget.find('.desc').html().split('<br>').map(x => '<p>' + x + '</p>')
+		+
+		`<p>
+			<a href='${tapTarget.find('address').text()}'>Image Source</a>
+		</p>`
+	);
+	let unit = tapTarget.find('.unit').text();
+	scroll.find('.info-unit').html(unit + ': ' + UNITS[unit]);
+	scroll.find('.info-region').html(tapTarget.find('.regions').text());
 	scroll.find('header').css('background-image', `url('${tapTarget.find('address').text()}')`);
 	scroll.removeClass('hide');
 	$('.container').addClass('popup-open');
@@ -12,49 +30,4 @@ $(document).on('click', '.bar, .dot', function (e) {
 $('article nav button').on('click', function (e) {
 	$('article').addClass('hide');
 	$('.container').removeClass('popup-open');
-});
-
-(function () {
-
-	var blurFilter = document.getElementById('blur-kernel');
-	var scrollView = $('.container');
-	var prevPos = $(window).scrollLeft();
-	var prevTime = Date.now();
-	var speed = 0;
-	var cheat = 0;
-	var stepper = 0;
-
-	function blur() {
-		stepper++;
-
-		if (stepper >= 3) {
-
-			stepper = 0;
-
-		  cheat = 1 - cheat; // alternates between 0 and 1; used to force browser repaint by setting CSS
-
-		  let newPos = $(window).scrollLeft();
-		  let newTime = Date.now()
-
-		  // distance over time times milliseconds per frame
-		  velocity = (newPos - prevPos) / Math.max(1, newTime - prevTime) * 1000 / 60;
-		  
-		  prevPos = newPos;
-		  prevTime = newTime;
-		  
-		  const STRENGTH = 2;
-		  blurSize = Math.max(0, Math.abs(velocity) / 2 * STRENGTH - 0.5);
-
-		  blurFilter.setStdDeviation(0, Math.floor(blurSize));
-		  blurFilter.setAttribute('stdDeviation', blurSize + ',0');
-
-		  // forces browser repaint
-		  scrollView.css('margin-bottom', -cheat + 'px');
-		 }
-	  
-	  requestAnimationFrame(blur);
-	}
-
-	requestAnimationFrame(blur);
-
 });
--- builder.js
+++ builder.js
@@ -1,94 +1,119 @@
 
-const GLOBAL_MIN = -11500;
+const GLOBAL_MIN = -11000;
 const GLOBAL_MAX = 2000;
 
-function yearify(year) {
+function yearify(year, html = false) {
 	if (year < 0) {
-		return -year + ' BCE';
+		return -year + (html ? ' <span>BC</span>' : ' BC');
 	}
 	else if (year > 0) {
-		return year + ' CE';
+		return year + (html ? ' <span>AD</span>' : ' AD');
 	}
 	else {
-		return 'BCE&nbsp;/&nbsp;CE'; // no year 0
+		return null; // no year 0
 	}
 }
 
-function plot(element, year, loc) {
-	element.append(`
-		<div class='marker' style='left: ${loc}vh;'>
-			<h3>${yearify(year)}</h3>
-		</div>
-	`);
+function plot(element, year, loc, major = true) {
+	if (year === 0) {
+		element.append(
+			`
+				<div class='marker supreme' style='left: ${loc}vh;'></div>
+			`
+		);
+		return;
+	}
+	element.append(
+		major ?
+		`
+			<div class='marker major' style='left: ${loc}vh;'>
+				<h3>${yearify(year, true)}</h3>
+			</div>
+		` : `
+			<div class='marker minor' style='left: ${loc}vh;'></div>
+		`
+	);
 }
 
 $.getJSON('timeline.json', function(timelineData) {
 
 	const SCALE = [
-		[-2500, 0.006, 1500],
-		[500, 0.04, 250],
-		[1250, 0.05, 250],
-		[Infinity, 0.3, 50]
+		[GLOBAL_MIN],
+		[-3000, 0.005, 2000, 200],
+		[1200, 0.05, 200, 50],
+		[Infinity, 0.25, 50, 25]
 	];
 
 	function widthCalc(begin, end) {
 		let Ccursor = begin;
-	  let Csegment = 0;
-	  let Clocator = 0;
+		let Csegment = 1;
+		let Clocator = 0;
 		while (true) {
-	  	Ccursor += 1;
-	  	Clocator += SCALE[Csegment][1];
-	  	if (Ccursor >= SCALE[Csegment][0]) {
-	  		Csegment++;
-	  	}
+			Ccursor += 1;
+			Clocator += SCALE[Csegment][1];
+			if (Ccursor >= SCALE[Csegment][0]) {
+				Csegment++;
+			}
 			if (Ccursor >= end) {
 				return Clocator;
 			}
-	  }
+		}
 	}
 
-  let cursor = GLOBAL_MIN;
-  let segment = 0;
-  let locator = 0;
+	let cursor = GLOBAL_MIN;
+	let segment = 1;
+	let locator = 0;
 
-  let plotNow = () => {plot($('main .markings'), cursor, locator)};
-  while (true) {
-  	plotNow();
-  	cursor += SCALE[segment][2];
-  	locator += SCALE[segment][1] * SCALE[segment][2];
-  	if (cursor >= SCALE[segment][0]) {
-  		segment++;
-  	}
-	if (cursor >= GLOBAL_MAX) {
-		break;
+	let plotNow = (major = true) => {plot($('main .markings'), cursor, locator, major)};
+	while (true) {
+		plotNow((cursor - SCALE[segment-1][0]) % SCALE[segment][2] === 0);
+		cursor += SCALE[segment][3];
+		locator += SCALE[segment][1] * SCALE[segment][3];
+		if (cursor >= SCALE[segment][0]) {
+			segment++;
+		}
+		if (cursor >= GLOBAL_MAX) {
+			break;
+		}
 	}
-  }
 
-  $('main').css('width', widthCalc(GLOBAL_MIN, GLOBAL_MAX) + 'vh');
+	$('.container, main').css('width', widthCalc(GLOBAL_MIN, GLOBAL_MAX) + 'vh');
 
-  for (let evt of timelineData.events) {
-  	if (evt.type === 'range') {
-	  	$(evt.region).append(`
-	  		<div class='bar' style='width: ${widthCalc(evt.begin, evt.end)}vh; top: ${evt.shift + 12}vh; left: ${widthCalc(GLOBAL_MIN, evt.begin)}vh;'>
-	  			<h2>${evt.name}</h2>
-	  			<p hidden>${evt.description}</p>
-	  			<address hidden>${evt.picture}</address>
-	  			<time hidden>${yearify(evt.begin)} &ndash; ${yearify(evt.end)}</time>
-	  		</div>
-	  	`);
-	  }
-	  else if (evt.type === 'point') {
-	  	$(evt.region).append(`
-	  		<div class='dot' style='top: ${evt.shift + 12}vh; left: ${widthCalc(GLOBAL_MIN, evt.at)}vh;'>
-	  			<div class='dot-details'>
-		  			<h2>${evt.name}</h2>
-	  				<p hidden>${evt.description}</p>
-	  				<address hidden>${evt.picture}</address>
-	  				<time hidden>${yearify(evt.at)}</time>
-		  		</div>
-	  		</div>
-	  	`);
-	  }
-  }
+	for (let evt of timelineData.events) {
+
+		let lineSelectors = evt.region.map(x => ({
+			'Africa &amp; the Islamic World' : '.s-africa-islamic',
+			'Asia' : '.s-china-india',
+			'Europe' : '.s-europe',
+			'The Americas' : '.s-americas'
+		}[x])).join(', ');
+
+		if (evt.type === 'range') {
+			$(lineSelectors).append(`
+				<div class='bar' style='width: ${widthCalc(evt.begin, evt.end)}vh; top: ${evt.shift + 12}vh; left: ${widthCalc(GLOBAL_MIN, evt.begin)}vh;'>
+					<h2>${evt.name}</h2>
+					<p hidden class='desc'>${evt.description}</p>
+					<address hidden>${evt.picture}</address>
+					<time hidden>${yearify(evt.begin)} &ndash; ${yearify(evt.end)}</time>
+					<p hidden class='regions'>${evt.region.join(', ')}</p>
+					<p hidden class='unit'>${evt.unit}</p>
+				</div>
+			`);
+		}
+		else if (evt.type === 'point') {
+			$(lineSelectors).append(`
+				<div class='dot' style='top: ${evt.shift + 12}vh; left: ${widthCalc(GLOBAL_MIN, evt.at)}vh;'>
+					<div class='dot-details'>
+						<h2>${evt.name}</h2>
+						<p hidden class='desc'>${evt.description}</p>
+						<address hidden>${evt.picture}</address>
+						<time hidden>${yearify(evt.at)}</time>
+						<p hidden class='regions'>${evt.region.join(', ')}</p>
+						<p hidden class='unit'>${evt.unit}</p>
+					</div>
+				</div>
+			`);
+		}
+	}
 
 });
